d5f744866a8504976e1d5b493f4b44042280aaa6
SORROW <linkinsweg42@gmail.com>
2024-10-21 23:57:37 +0300

    fixed favicon

--- modules/header.php
+++ modules/header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="assets/icons/favicon.ico" type="image/x-icon">
-    <link rel="shortcut icon" href="assets/icons/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/assets/icons/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/assets/icons/favicon.ico" type="image/x-icon">
     <title>Коптра - Производство и продажа дронов</title>
     <link
         rel="stylesheet"
