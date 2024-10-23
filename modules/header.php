6fbf82f5f51ab613a203fbae57202f1ad5b6963c
SORROW <linkinsweg42@gmail.com>
2024-10-23 23:26:22 +0300

    FUCK THIS SHIT REVERT TO MISHA

--- modules/header.php
+++ modules/header.php
@@ -11,6 +11,7 @@
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
     />
     <link rel="stylesheet" href="/styles/main.css" />
+    <link rel="stylesheet" href="/styles/components/sidebar.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </head>
@@ -24,12 +25,41 @@
                 <ul>
                     <li><a href="../index.php">Главная</a></li>
                     <li class="dropdown">
-                        <a href="products.html">Продукты</a>
+                        <a href="products.php">Продукты</a>
                         <ul class="dropdown-menu">
-                            <li><a href="products.html#koptra-mini">Коптра Мини</a></li>
-                            <li><a href="products.html#koptra-basic">Коптра Базовый</a></li>
-                            <li><a href="products.html#accessories">Аксессуары</a></li>
-                            <li><a href="products.html#spare-parts">Запчасти</a></li>
+                            <li class="dropdown-submenu">
+                                <a href="products.php#agriculture">Сельское хозяйство</a>
+                                <div class="submenu">
+                                    <a href="products.php#agro-scout">Агроскаут X1</a>
+                                    <a href="products.php#agro-scout-pro">Агроскаут X1 Pro</a>
+                                    <a href="products.php#agro-mapper">Агроскаут Картограф</a>
+                                    <a href="products.php#agro-sprayer">Агроскаут Опрыскиватель</a>
+                                </div>
+                            </li>
+                            <li class="dropdown-submenu">
+                                <a href="products.php#education">Образование</a>
+                                <div class="submenu">
+                                    <a href="products.php#edu-basic">Коптра Пионер</a>
+                                    <a href="products.php#edu-advanced">Коптра Пионер Pro</a>
+                                    <a href="products.php#edu-kit">Образовательный набор</a>
+                                </div>
+                            </li>
+                            <li class="dropdown-submenu">
+                                <a href="products.php#fpv">FPV</a>
+                                <div class="submenu">
+                                    <a href="products.php#fpv-racer">FPV Racer 5"</a>
+                                    <a href="products.php#fpv-freestyle">FPV Freestyle</a>
+                                    <a href="products.php#fpv-cinewhoop">Cinewhoop 3"</a>
+                                </div>
+                            </li>
+                            <li class="dropdown-submenu">
+                                <a href="products.php#cargo">Грузовые</a>
+                                <div class="submenu">
+                                    <a href="products.php#cargo-light">Грузовой X4</a>
+                                    <a href="products.php#cargo-medium">Грузовой X6</a>
+                                    <a href="products.php#cargo-heavy">Грузовой X8</a>
+                                </div>
+                            </li>
                         </ul>
                     </li>
                     <li><a href="/docs/index.php">Документация</a></li>
@@ -37,6 +67,5 @@
                     <li><a href="contacts.html">Контакты</a></li>
                 </ul>
             </nav>
-        <?php endif; ?>
-    </div>
-</header>
+        </div>
+    </header>
