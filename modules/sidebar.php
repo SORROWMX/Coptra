a9cf0cb6c7dd089847f2aaf017bebe72ba743385
M_ANTIS <erohin.stanislav2017@yandex.ru>
2024-10-25 23:03:52 +0300

    added sidebar docs

--- modules/sidebar.php
+++ modules/sidebar.php
@@ -2,35 +2,114 @@
     <nav class="sidebar-nav">
         <div class="sidebar-section">
             <h3>РУКОВОДСТВА:</h3>
-            <div class="sidebar-item">
-                <a href="#" class="sidebar-link">I. Введение</a>
-            </div>
-            <div class="sidebar-item expandable">
-                <span class="sidebar-link">
-                    Приступая к работе
-                    <span class="arrow"></span>
-                </span>
-                <ul class="sub-menu extended-submenu">
-                    <li><a href="setup">Руководство по настройке</a></li>
-                    <li><a href="hardware">Аппаратное обеспечение</a></li>
-                    <li><a href="troubleshooting">Устранение неполадок</a></li>
-                </ul>
-            </div>
         </div>
         <div class="sidebar-item">
-            <a href="release-notes" class="sidebar-link">Примечания к выпуску</a>
+            <a href="#" class="sidebar-link">Введение</a>
+        </div>
+        <!-- =<div class="sidebar-item">
+            <a href="boards" class="sidebar-link">Полётный контроллер</a>
+        </div> -->
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Обучающие дроны
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="setup">Дрон1</a></li>
+                <li><a href="hardware">Дрон2</a></li>
+                <li><a href="troubleshooting">Дрон3</a></li>
+            </ul>
+        </div>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Сельскохозяйственные дроны
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="setup">Дрон1</a></li>
+                <li><a href="hardware">Дрон2</a></li>
+                <li><a href="troubleshooting">Дрон3</a></li>
+            </ul>
+        </div>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                FPV дроны
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="setup">Дрон1</a></li>
+                <li><a href="hardware">Дрон2</a></li>
+                <li><a href="troubleshooting">Дрон3</a></li>
+            </ul>
+        </div>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Грузовые дроны
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="setup">Дрон1</a></li>
+                <li><a href="hardware">Дрон2</a></li>
+                <li><a href="troubleshooting">Дрон3</a></li>
+            </ul>
         </div>
         <div class="sidebar-item">
-            <a href="configuration" class="sidebar-link">Примечания по настройке</a>
+            <a href="boards" class="sidebar-link">Пульт радиоуправления</a>
         </div>
-        <div class="sidebar-item">
-            <a href="manuals" class="sidebar-link">Руководство</a>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Системы навигации
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="setup">Система ИК навигации</a></li>
+                <li><a href="hardware">Система УЗ навигации</a></li>
+                <li><a href="troubleshooting">УСиcтема оптической навигации</a></li>
+            </ul>
         </div>
-        <div class="sidebar-item">
-            <a href="configurator" class="sidebar-link">Конфигуратор</a>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Полезная нагрузка
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="setup">Плата подключения дополнительных модулей</a></li>
+                <li><a href="hardware">Конфигурация</a></li>
+                <li><a href="troubleshooting">Управление</a></li>
+            </ul>
         </div>
-        <div class="sidebar-item">
-            <a href="boards" class="sidebar-link">Доски объявлений</a>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Программное обеспечение
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="pioneer-station">Pioneer Station</a></li>
+                <li><a href="pioneer-drone-sim">Pioneer Drone Sim</a></li>
+            </ul>
+        </div>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Программирование
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="python">Python</a></li>
+                <li><a href="trik-studio">TRIK Studio</a></li>
+                <li><a href="lua">LUA</a></li>
+                <li><a href="pioneer-max-ros">Пионер Макс + ROS</a></li>
+            </ul>
+        </div>
+        <div class="sidebar-item expandable">
+            <span class="sidebar-link">
+                Дополнительные материалы
+                <span class="arrow"></span>
+            </span>
+            <ul class="sub-menu extended-submenu">
+                <li><a href="knowledge-base">База знаний</a></li>
+                <li><a href="downloads">Загрузки</a></li>
+                <li><a href="methods-and-cases">Методики и кейсы</a></li>
+            </ul>
         </div>
     </nav>
 </aside>
