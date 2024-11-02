07d956981b4c44c94ec968d06c175835b902dc12
M_ANTIS <erohin.stanislav2017@yandex.ru>
2024-11-02 18:08:46 +0300

    Revert "fixed docs header (without a sidebar)"
    This reverts commit f350ce30458452542db49fc72d9effa69c7899a7.

--- modules/header.php
+++ modules/header.php
@@ -1,4 +1,5 @@
 <?php
+// В начале файла добавьте:
 $root_path = '/'; // Измените на '/your-subdirectory/' если сайт находится в поддиректории
 ?>
 
@@ -57,18 +58,75 @@ $root_path = '/'; // Измените на '/your-subdirectory/' если сай
 </div>
 <header>
     <div class="container header-container">
-            <a href="<?php echo $root_path; ?>index.php" class="logo">
-                <img src="<?php echo $root_path; ?>assets/images/header-logo.png" alt="Коптра лого" />
-                <span>КОПТРА</span>
-            </a>
+        <a href="<?php echo $root_path; ?>index.php" class="logo">
+            <img src="<?php echo $root_path; ?>assets/images/header-logo.png" alt="Коптра лого" />
+            <span>КОПТРА</span>
+        </a>
         <?php 
-        // Определяем, находимся ли мы на странице документации
-        $current_path = $_SERVER['PHP_SELF'] ?? '';
-        $is_docs_page = strpos($current_path, 'docs/') !== false;
+        $current_file = $_SERVER['SCRIPT_FILENAME'];
         
-        if (!$is_docs_page): 
+        if (strpos(strtolower($current_file), 'docs') !== false): 
         ?>
-            <!-- Обычное меню для всех страниц кроме документации -->
+            <!-- СТРАНИЦА ДОКУМЕНТАЦИИ ДЛЯ ДЕСКТОПА -->
+            <nav class="docs-nav desktop-only">
+                <ul>
+                    <li><a href="<?php echo $root_path; ?>index.php">Гойда</a></li>
+                    <li class="dropdown">
+                        <a href="<?php echo $root_path; ?>products.php">Продукты</a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-submenu">
+                                <a href="products.php#agriculture">Сельское хозяйство</a>
+                                <div class="submenu">
+                                    <a href="products.php#agro-scout">Агроскаут X1</a>
+                                    <a href="products.php#agro-scout-pro">Агроскаут X1 Pro</a>
+                                    <a href="products.php#agro-mapper">Агроскаут Картограф</a>
+                                    <a href="products.php#agro-sprayer">Агроскаут Опрыскиватель</a>
+                                </div>
+                            </li>
+                            <li class="dropdown-submenu">
+                                <a href="products.php#education">Образование</a>
+                                <div class="submenu">
+                                    <a href="products.php#edu-basic">Коптра Пионер</a>
+                                    <a href="products.php#edu-advanced">Коптра Пионер Pro</a>
+                                    <a href="products.php#edu-kit">Образовательный набор</a>
+                                </div>
+                            </li>
+                            <li class="dropdown-submenu">
+                                <a href="products.php#fpv">FPV</a>
+                                <div class="submenu">
+                                    <a href="products.php#fpv-racer">FPV Racer 5"</a>
+                                    <a href="products.php#fpv-freestyle">FPV Freestyle</a>
+                                    <a href="products.php#fpv-cinewhoop">Cinewhoop 3"</a>
+                                </div>
+                            </li>
+                            <li class="dropdown-submenu">
+                                <a href="products.php#cargo">Грузовые</a>
+                                <div class="submenu">
+                                    <a href="products.php#cargo-light">Грузовой X4</a>
+                                    <a href="products.php#cargo-medium">Грузовой X6</a>
+                                    <a href="products.php#cargo-heavy">Грузовой X8</a>
+                                </div>
+                            </li>
+                            <li><a href="products.php#flight-controller">Полётный контроллер</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="<?php echo $root_path; ?>docs/documentation.php">Документация</a></li>
+                    <li><a href="<?php echo $root_path; ?>blog.html">Блог</a></li>
+                    <li><a href="<?php echo $root_path; ?>contacts.html">Контакты</a></li>
+                </ul>
+            </nav>
+            <button class="sidebar-mobile-btn header-btn" aria-label="Toggle sidebar">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        <?php else: ?>
+            <button class="mobile-menu-btn">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            <!-- ГЛАВНАЯ СТРАНИЦА ДЛЯ ДЕСКТОПА И МОБИЛКИ -->
             <nav class="main-nav">
                 <ul>
                     <li><a href="<?php echo $root_path; ?>index.php">Главная</a></li>
@@ -116,18 +174,6 @@ $root_path = '/'; // Измените на '/your-subdirectory/' если сай
                     <li><a href="<?php echo $root_path; ?>contacts.html">Контакты</a></li>
                 </ul>
             </nav>
-            <button class="mobile-menu-btn">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
-        <?php else: ?>
-            <!-- Специальная кнопка для документации -->
-            <button class="sidebar-mobile-btn header-btn">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
         <?php endif; ?>
     </div>
 </header>
