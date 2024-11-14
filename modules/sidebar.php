c4462e28f1cd3870633fc4611621871fdbd9833c
SORROW <linkinsweg42@gmail.com>
2024-11-14 21:35:39 +0300

    Revert "Added a search to the documentation"
    This reverts commit 5fafa24eebb47943286d3640f053f169f26ccef4.

--- modules/sidebar.php
+++ modules/sidebar.php
@@ -30,16 +30,6 @@ if (!$is_local) {
 }
 ?>
 <aside class="sidebar">
-    <div class="search-container">
-        <div class="search-wrapper">
-            <input type="text" id="docs-search" class="search-input" placeholder="Поиск по документации...">
-            <button type="button" id="search-button" class="search-button">
-                <svg class="search-icon" viewBox="0 0 24 24">
-                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                </svg>
-            </button>
-        </div>
-    </div>
     <nav class="sidebar-nav">
         <div class="sidebar-section">
             <h3>РУКОВОДСТВА:</h3>
